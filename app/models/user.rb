cc49609c14c91164dbcb3e5eb368abc7a7467c17
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-04-01 23:13:34 +0900

    nicknameのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-  validates :age,  presence: true
+  validates :nickname, presence: true
 end
