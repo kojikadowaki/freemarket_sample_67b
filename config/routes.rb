cc588b94e1804c1d38f52c5ff2e43bd43e94be76
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-04-05 17:22:53 +0900

    ユーザ登録用のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get  'profiles',           to: 'users/registrations#new_profile'
+    post 'profiles',           to: 'users/registrations#create_profile'
+    get  'shipping_addresses', to: 'users/registrations#new_address'
+    post 'shipping_addresses', to: 'users/registrations#create_address'
+  end
 end
