aeb1cac18ece7b5c0316a374d8c00c768fa569f1
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-04-05 16:27:30 +0900

    アソシエーションの微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  nickname               :string(255)      not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
+  has_one :profile,          dependent: :destroy
+  has_one :shipping_address, dependent: :destroy
+  
+  validates :nickname,
+            presence: true,
+            length: { maximum: 10 }
 end
