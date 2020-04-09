8f40fcf03c1a4ac5cfeee98c088ead415b9ca46e
kaito0402 <kaitoseibo04@gmail.com>
2020-04-09 23:55:12 +0900

    active_hashで作成したデータ

--- app/models/derivery_day.rb
+++ app/models/derivery_day.rb
@@ -1,8 +1,6 @@
-
 class DeriveryDay < ActiveHash::Base
   self.data = [
     {id: 1, derivery_day: '1~2日で発送'}, {id: 2, derivery_day: '2~3日で発送'},
     {id: 3, derivery_day: '4~7日で発送'}
   ]
-
 end
--- app/models/ship_from_location.rb
+++ app/models/ship_from_location.rb
@@ -17,4 +17,5 @@ class ShipFromLocation < ActiveHash::Base
     {id: 43, name: '熊本県'}, {id: 44, name: '大分県'}, {id: 45, name: '宮崎県'},
     {id: 46, name: '鹿児島県'}, {id: 47, name: '沖縄県'}, {id: 48, name: '未定'}
   ]
+
 end
