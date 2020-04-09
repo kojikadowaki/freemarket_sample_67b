ef520038546368aa3698824af4604e526d3329a1
kaito0402 <kaitoseibo04@gmail.com>
2020-04-09 23:41:50 +0900

    active_hashでの実装のためにマイグレーションファイルを消去

--- db/migrate/20200405043653_create_derivery_days.rb
+++
@@ -1,8 +0,0 @@
-class CreateDeriveryDays < ActiveRecord::Migration[5.0]
-  def change
-    create_table :derivery_days do |t|
-      t.integer :derivery_day, null: false
-      t.timestamps
-    end
-  end
-end
