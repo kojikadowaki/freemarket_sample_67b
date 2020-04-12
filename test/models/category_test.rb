4e5cb0b64390aacbd1d0067e6a90b3bb5030b4c2
shota-code <60507556+shota-code@users.noreply.github.com>
2020-04-12 18:36:01 +0900

    Merge branch 'category_function' of https://github.com/kojikadowaki/freemarket_sample_67b into category_function

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: categories
-#
-#  id         :integer          not null, primary key
-#  ancestry   :string(255)
-#  name       :string(255)      not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_categories_on_ancestry  (ancestry)
-#
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
