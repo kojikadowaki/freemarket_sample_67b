ba1fea6d11973f68ac5d88d7dfcebe2a6dfbe6a3
kaito0402 <kaitoseibo04@gmail.com>
2020-04-17 19:35:58 +0900

    マイグレーションファイルの修正、消去

--- db/migrate/20200405041713_create_sizes.rb
+++ db/migrate/20200405041713_create_sizes.rb
@@ -1,7 +1,7 @@
 class CreateSizes < ActiveRecord::Migration[5.0]
   def change
     create_table :sizes do |t|
-      t.string :size, nill: false
+      t.string :size, null: false
       t.timestamps
     end
   end
