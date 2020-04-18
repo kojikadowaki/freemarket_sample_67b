a3a527d4e4d5bb3f76eaa0c9e6bd9d2c55105d51
kaito0402 <kaitoseibo04@gmail.com>
2020-04-18 16:35:22 +0900

    マイグレーションファイルの変更

--- db/migrate/20200405051855_create_products.rb
+++ db/migrate/20200405051855_create_products.rb
@@ -4,13 +4,13 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.string :name, null: false
       t.integer :price, null: false
       t.text :description, null:false
-      t.references :size, null: false, foreign_key: true
-      t.integer :ship_from_location_id	
-      t.integer :product_condition_id	
-      t.integer :derivery_fee_payer_id
-      t.integer :derivery_day_id
-      t.references :category, foreign_key: true
-      t.references :user, foreign_key: true
+      t.references :size, foreign_key: true
+      t.integer :ship_from_location_id, null: false
+      t.integer :product_condition_id,	null: false
+      t.integer :derivery_fee_payer_id, null: false
+      t.integer :derivery_day_id, null: false
+      t.references :category, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false,
       t.timestamps
     end
   end
