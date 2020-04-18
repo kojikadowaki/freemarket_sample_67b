7fb9ec2770eec823172d79290cece5f5161c53f4
kaito0402 <kaitoseibo04@gmail.com>
2020-04-18 16:38:41 +0900

    derivery_methodアクティブハッシュの値変更

--- app/models/derivery_method.rb
+++ app/models/derivery_method.rb
@@ -1,6 +1,6 @@
 class DeriveryMethod < ActiveHash::Base
   self.data = [
     {id: 1, method: 'A便'}, {id: 2, method: 'B便'}, {id: 3, method: 'C便'},
-    {id: 4, method: 'D便'},  {id: 5, method: 'E便'}
+    {id: 4, method: 'D便'}, {id: 5, method: 'E便'}
   ]
 end
