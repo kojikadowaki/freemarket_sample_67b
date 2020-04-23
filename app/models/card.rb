600b98d7d63d01aefec336bd9cd23fd063e26f36
CrazyForTimtam <2111111@kuis.ac.jp>
2020-04-24 03:59:24 +0900

    cardモデルの作成

---
+++ app/models/card.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: cards
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  card_id     :string(255)
+#  customer_id :string(255)
+#  user_id     :integer
+#
+class Card < ApplicationRecord
+  belongs_to :user
+end
