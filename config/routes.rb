f52d47c9cfd8f5f681e7b73604f2cf7b445dfc46
CrazyForTimtam <2111111@kuis.ac.jp>
2020-04-24 15:14:34 +0900

    cardのresoucesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -26,12 +26,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :card, only: [:new, :show] do
+  resources :card, only: [:new, :show, :destroy] do 
     collection do
-      post 'show',   to: 'card#show'
       post 'pay',    to: 'card#pay'
-      post 'delete', to: 'card#delete'
     end
   end
-  
+
 end
