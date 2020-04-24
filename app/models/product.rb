c90ce339d47fa43f7d37e2b5cb51f4c30f22fc33
kojikadowaki <gbsg9405@yahoo.co.jp>
2020-04-24 18:57:24 +0900

    マイグレーションファイル修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,36 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                    :integer          not null, primary key
+#  brand                 :string(255)
+#  description           :text(65535)      not null
+#  name                  :string(255)      not null
+#  price                 :integer          not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  category_id           :integer          not null
+#  derivery_day_id       :integer          not null
+#  derivery_fee_payer_id :integer          not null
+#  derivery_method_id    :integer          not null
+#  product_condition_id  :integer          not null
+#  product_status_id     :integer          not null
+#  ship_from_location_id :integer          not null
+#  size_id               :integer
+#  user_id               :integer          not null
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
+#  index_products_on_size_id      (size_id)
+#  index_products_on_user_id      (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (size_id => sizes.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :size, optional: true
--- db/migrate/20200420140227_add_product_status_id_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddProductStatusIdToProduct < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :product_status_id, :integer
-  end
-end
