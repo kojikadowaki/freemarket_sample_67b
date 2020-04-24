4ad5a0715b036f907d139764a65fa566e51fc931
CrazyForTimtam <2111111@kuis.ac.jp>
2020-04-24 13:49:18 +0900

    where.firstをfind_byに変更
    deleteメソッド内の修正

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -28,7 +28,7 @@ class CardController < ApplicationController
   end
 
   def delete #PayjpとCardデータベースを削除
-    card = Card.where(user_id: current_user.id).first
+    card = Card.find_by(user_id: current_user.id)
     if card.blank?
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
