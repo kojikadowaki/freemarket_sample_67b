53db802de19369dcca8d50b403f694170e70c8a1
CrazyForTimtam <2111111@kuis.ac.jp>
2020-05-07 14:34:38 +0900

    商品購入機能の追加
    商品購入機能に関するcontroller、view、及び生成時に作成されたファイル複数

---
+++ app/controllers/purchase_controller.rb
@@ -0,0 +1,52 @@
+class PurchaseController < ApplicationController
+  
+  def index #購入内容確認画面
+    @user    = current_user
+    @card    = Card.find_by(user_id: current_user.id)
+    @product = Product.find(params[:id])
+    if @card.present?
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer                  = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+      @card_brand               = @default_card_information.brand 
+      case @card_brand
+      when "Visa"
+        @card_src = "card_logo_visa.png"
+      when "MasterCard"
+        @card_src = "card_logo_master.png"
+      when "Saison"
+        @card_src = "card_logo_saisonr.png"
+      when "JCB"
+        @card_src = "card_logo_jcb.gif"
+      when "American Express"
+        @card_src = "card_logo_amex.gif"
+      end
+    else
+      redirect_to new_card_path
+    end
+  end
+
+  def pay #商品購入メソッド
+    @product      = Product.find(params[:id])
+    @card         = Card.find_by(user_id: current_user.id)
+    Payjp.api_key = Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+
+    Payjp::Charge.create(
+      :amount   => @product.price,
+      :customer => @card.customer_id,
+      :currency => 'jpy', 
+    )
+
+    @product.update(status: "購入済み" )
+    Order.create!(buyer_user_id: current_user.id, product_id: @product.id)
+
+
+    redirect_to done_purchase_path #購入完了画面へ遷移
+  end
+
+  def done #商品購入完了画面
+    @categories = Category.eager_load(children: :children).where(ancestry: nil)
+    @product    = Product.find(params[:id])
+  end
+  
+end
