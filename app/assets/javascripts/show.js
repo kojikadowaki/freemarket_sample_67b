899c20e7e82b6445d5767520ad5b5f1ba0512ad6
shota-code <60507556+shota-code@users.noreply.github.com>
2020-05-14 18:19:06 +0900

    show_markup

---
+++ app/assets/javascripts/show.js
@@ -0,0 +1,9 @@
+$(window).on('load', function() {
+  $('img.top_content__product_box__image--image_list').mouseover(function () {
+    var $scr = $(this).attr('src');
+    $('.top_content__product_box__image--main_image').attr('src', $scr);
+    console.log(this)
+  });
+
+});
+
--- spec/models/size_spec.rb
+++ spec/models/size_spec.rb
@@ -4,7 +4,7 @@
 #
 #  id         :integer          not null, primary key
 #  ancestry   :string(255)
-#  size       :string(255)      default("")
+#  size       :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
