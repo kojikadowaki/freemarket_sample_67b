7deaee67a22210614502366f0e407bd208dd11c2
CrazyForTimtam <2111111@kuis.ac.jp>
2020-05-20 17:22:42 +0900

    ordersテーブルのカラム名一部変更
    ordersテーブル（usersテーブルとproductsテーブルの中間テーブル）の、カラム「buyer_user_id」を「user_id」に変更。それに伴い、ordersテーブルに関わるファイルも修正。

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   has_one :profile,          dependent: :destroy
   has_one :shipping_address, dependent: :destroy
   has_many :comments
-  has_many :orders, class_name: "Order", foreign_key: "buyer_user_id"
+  has_many :orders, class_name: "Order", foreign_key: "user_id"
   has_many :products, through: :ordres
   validates :nickname,
             presence: true,
