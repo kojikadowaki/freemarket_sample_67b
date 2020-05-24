20656a4852a80deacbd8ae94e360f8e6b0542535
kojikadowaki <gbsg9405@yahoo.co.jp>
2020-05-24 23:16:55 +0900

    products_controller 単体テスト

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 
 describe ProductsController do
   describe 'GET #search' do
-    context "検索ヒットした場合" do
+    context "商品名で検索ヒットした場合" do
       before do
-        @product = create(:product, name: 'グッズ')
+        @product = create(:product, name: 'ONE PICE', description: '面白い')
       end
         it "@productsに正しい値が入っていること" do
-          get :search, params: { q: { name: "グッズ"} }
+          get :search, params: { q: { name: "ONE PICE"} }
           expect(assigns(:products)).to match_array(@product)
         end
         it "search.html.hamlに遷移すること" do
@@ -16,13 +16,27 @@ describe ProductsController do
           expect(response).to render_template :search
         end
     end
-    context "検索ヒットしなかった場合" do
+    context "商品説明で検索ヒットした場合" do
+      it "@productsに正しい値が入っていること" do
+        get :search, params: { q: { description: "面白い"} }
+        expect(assigns(:products)).to match_array(@product)
+      end
+      it "search.html.hamlに遷移すること" do
+        get :search
+        expect(response).to render_template :search
+      end
+    end
+    context "商品名で検索ヒットしなかった場合" do
       it "@productsに正しい値が入っていること" do
         get :search, params: { q: { name: "グッズ"} }
         expect(assigns(:products)).to match_array([])
       end
     end
-  
-  
+    context "商品説明で検索ヒットしなかった場合" do
+      it "@productsに正しい値が入っていること" do
+        get :search, params: { q: { description: "グッズ"} }
+        expect(assigns(:products)).to match_array([])
+      end
+    end
   end
 end
