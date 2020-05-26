0d14a5113700371bf8e49a9e3cd470bff7889e9c
shota-code <60507556+shota-code@users.noreply.github.com>
2020-05-26 17:14:38 +0900

    修正３

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
+    comment = Comment.new(comment_params)
     comment_id = comment.product.id
     if comment.save
       redirect_to product_path(comment_id)
